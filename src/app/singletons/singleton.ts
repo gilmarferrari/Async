f6f718a29e05586218bfe87dcf97e73528c4e76b
gilmarcoop <93219984+gilmarcoop@users.noreply.github.com>
2022-07-28 14:05:52 -0300

    Update singleton.ts

--- src/app/singletons/singleton.ts
+++ src/app/singletons/singleton.ts
@@ -1,5 +1,7 @@
 import { AuthorizationType } from "../home/home.component";
 
+
+// Dont use singletons, man
 export class Singleton {
     public static AuthorizationType: AuthorizationType = AuthorizationType.None;
     public static Token: any;
